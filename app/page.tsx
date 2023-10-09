5602e912474124ef5247efa0498db9e1b3fea42d
Motoaki Tanaka <katachiwotodomete@gmail.com>
2023-10-09 01:06:05 +0000

    width調整

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Page() {
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-center px-4 mt-12 sm:mt-20 max-w-xl">
+      <main className="flex flex-1 w-full flex-col items-center justify-center px-4 mt-12 sm:mt-20 max-w-2xl">
         <Image
           width={200}
           height={200}
